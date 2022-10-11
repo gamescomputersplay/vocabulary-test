7ca63038a562c9a8692493031efaebc2ec5236d6
gamescomputersplay <kvasov.dmi@gmail.com>
2022-10-12 00:49:50 +0800

    Detect if use API or local data

--- src/Consts.js
+++ src/Consts.js
@@ -1,4 +1,9 @@
 const levels = [ 'A1', 'A2', 'B1', 'B2', 'C1', 'C2' ]
-const useLocalData = false
+let useLocalData = true
+
+// When published, use real API
+if (document.location.href.includes("how-many-words-do-you-know.com")){
+    useLocalData = false
+}
 
 export {levels, useLocalData }
