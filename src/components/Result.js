bc139d2836d3579c95bf9e729ad01d70bca21aa9
gamescomputersplay <kvasov.dmi@gmail.com>
2022-11-22 01:25:29 +0800

    Share results on Twitter

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,13 +3,16 @@ import Diagram from './Diagram'
 import {langName, levels} from '../Consts'
 
 import ReactGA from "react-ga4";
+import { TwitterShareButton, TwitterIcon } from "react-share";
+
 
 class Result extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             showButton: false
+            showButton: false,
+            showShare: false
         }
     }
         
@@ -27,6 +30,12 @@ class Result extends Component {
             ),
             1000
         )
+        setTimeout (
+            () => this.setState(
+                {showShare: true}
+            ),
+            500
+        )
         ReactGA.event({
             category: 'results',
             action: 'level',
@@ -41,18 +50,39 @@ class Result extends Component {
 
 
     render() {
+        
+        let shareUrl = "http://how-many-words-do-you-know.com"
+        let shareText = "I know approximately " + this.props.vocabSize + " " + langName[this.props.language] 
+                        + " words, my vocabulary level is " + this.levelMessage(this.props.result) +". Tested with "
+
         var button = null
+        var share = null
+
         if (this.state.showButton) {
             button = <button className="button-again" onClick={() => this.props.goAgain(this.props.language)}>Take the test again</button>
+        }
+        if (this.state.showShare) {
+            share = (
+            <span className="social-buttons">
+                <TwitterShareButton url={shareUrl} title={shareText}>
+                    <TwitterIcon size="30" round="True" />
+                </TwitterShareButton>
+            </span>
+            )
         }
 
         return (
             <div>
-                <h2 class="cefr">Your vocabulary level in {langName[this.props.language]} is {this.levelMessage(this.props.result)}</h2>
+                <h2 class="cefr">You know approximately {this.props.vocabSize} {langName[this.props.language]} words. Your vocabulary level is {this.levelMessage(this.props.result)}
+                &nbsp;{share}
+                </h2>
+                
+
                 <p class="cefrlink">You can read more about CEFR levels <a href="https://en.wikipedia.org/wiki/Common_European_Framework_of_Reference_for_Languages">here</a>.</p>
-                <h2>You know approximately {this.props.vocabSize} words</h2>
+                
                 <Diagram known={this.props.knownWords} unknown={this.props.unknownWords} />
                 {button}
+                
             </div>
         )
     }
